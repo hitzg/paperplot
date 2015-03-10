f6312561574a5b60268d24553e08cfd35cdedb53
Gregory Hitz <hitzg@ethz.ch>
2015-03-10 14:03:41 +0100

    renamed main file and added setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='paperplot',
+      version='1.0',
+      description='Simple settings for publication figures',
+      author='Gregory Hitz',
+      py_modules=['paperplot'],
+      )
